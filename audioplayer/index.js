b62787b2948c3c9920929fae49c1c4e8314c0144
Pauleg7 <p.s.84@mail.ru>
2018-05-29 00:53:11 +0300

    finish edit audioplayer

--- audioplayer/index.js
+++ audioplayer/index.js
@@ -6,14 +6,12 @@ let allMusic = {
 'This is it band' : 'mp3/This is it band.mp3'
 };
 
-
 const mediaplayer = document.getElementsByClassName('mediaplayer')[0];
 const playstate = document.getElementsByClassName('playstate')[0];
 const audio = mediaplayer.getElementsByTagName('audio')[0];
 const btnStop = document.getElementsByClassName('stop')[0];
 const btnBack = document.getElementsByClassName('back')[0];
 const btnNext = document.getElementsByClassName('next')[0];
-
 
 playstate.onclick = function() {
   if (audio.paused) {
